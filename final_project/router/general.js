5e4ccef05f3295eecc3c86a0140740683f31b532
loganlang393 <langewischl@gmail.com>
2025-08-19 23:31:12 -0400

    Added in the promises for the function to retrieve books from an author

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -31,21 +31,44 @@ public_users.get('/isbn/:isbn',function (req, res) {
     if(books[isbn]){
         resolve(books[isbn]);
     }else {
-        reject("Could not find a book with this isbn");
+        reject("Could not find a book with the isbn " + isbn);
     }
   });
 
   myPromise.then((successMessage) => {
     return res.status(300).send(successMessage);
   }, (failMessage) => {
-    return res.status(300).json({message: "Failed to retrieve list of books"});
+    return res.status(300).json({message: failMessage});
   });
  });
   
 // Get book details based on author
 public_users.get('/author/:author',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+    const author = req.params.author;
+
+    let myPromise = new Promise((resolve, reject) => {
+        const keys = Object.keys(books);
+
+        let authorBooks = {};
+
+        keys.forEach((key) => {
+            if(books[key].author === author){
+                authorBooks[key] = books[key];
+            }
+        })
+
+        if(Object.keys(authorBooks)){
+            resolve(JSON.stringify(authorBooks, null, 4));
+        }else {
+            reject("Could not find books from the author " + author);
+        }
+      });
+    
+      myPromise.then((successMessage) => {
+        return res.status(300).send(successMessage);
+      }, (failMessage) => {
+        return res.status(300).json({message: failMessage});
+      });
 });
 
 // Get all books based on title
